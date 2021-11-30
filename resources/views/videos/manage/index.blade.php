a1beaa5b6cacdb531492058dd8bc048c279935af
daudi44 <daudi@iesebre.com>
2021-11-30 17:36:16 +0100

    Petits retocs de la secció add, després d'haver vist el vídeo 117, a més a més, fix d'algúns errors de la barra de navegació responsive

--- resources/views/videos/manage/index.blade.php
+++ resources/views/videos/manage/index.blade.php
@@ -24,13 +24,19 @@
                                     </div>
                                 </div>
 
-                                <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                                <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5 pb-6">
                                     <label for="url" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">Url</label>
                                     <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                        <input type="text" name="url" id="url" class="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md my-2">
+                                        <div class="max-w-lg flex rounded-md shadow-sm">
+                                              <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm ">
+                                                https://
+                                              </span>
+                                                <input type="text" name="url" id="url" class="flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300">
+
+                                        </div>
                                     </div>
                                 </div>
-                                <button type="button" class="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                <button type="button" class="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-500 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
                                     Add
                                 </button>
                             </form>
