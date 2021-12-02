a0efbb0869d8ee2a05174484526889d565bf5017
daudi44 <daudi@iesebre.com>
2021-12-02 17:41:29 +0100

    Creo un component a banda per a la barra d'estat que apareix quan creem/borrem un usuari o video

--- resources/views/videos/manage/index_usuaris.blade.php
+++ resources/views/videos/manage/index_usuaris.blade.php
@@ -2,40 +2,9 @@
 
     <!-- This example requires Tailwind CSS v2.0+ -->
     <div class="flex flex-col">
-
-        @if(session()->has('success') or session()->has('status'))
-            <div class="rounded-md bg-green-100 p-4">
-                <div class="flex">
-                    <div class="flex-shrink-0">
-                        <!-- Heroicon name: solid/check-circle -->
-                        <svg class="h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                        </svg>
-                    </div>
-                    <div class="ml-3">
-                        <p class="text-sm font-medium text-green-800">
-                            {{session('success')}}
-                            {{session('status')}}
-                        </p>
-                    </div>
-                    <div class="ml-auto pl-3">
-                        <div class="-mx-1.5 -my-1.5">
-                            <button type="button" class="inline-flex bg-green-50 rounded-md p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-50 focus:ring-green-600">
-                                <span class="sr-only">Dismiss</span>
-                                <!-- Heroicon name: solid/x -->
-                                <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        @endif
-
-
+        <x-estat></x-estat>
             @can('users_manage_create')
-                <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
+                <div class="mx-auto w-full max-w-6xl bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
                     <div class="-ml-4 -mt-4 flex justify-between items-center flex-wrap sm:flex-nowrap">
                         <div class="ml-4 mt-4">
                             <h3 class="text-lg leading-6 font-medium text-gray-900 pb-3">
@@ -71,7 +40,7 @@
                 </div>
             @endcan
 
-        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="overflow-x-auto">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                 <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                     <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6 mt-6">
@@ -106,8 +75,8 @@
                         @endforeach
                         </tbody>
                     </table>
-                </div>
             </div>
         </div>
+    </div>
     </div>
 </x-casteaching-layout>
