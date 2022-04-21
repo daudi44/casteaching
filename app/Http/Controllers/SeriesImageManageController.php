f6633190f5be89de1e73fed8e1900a3e122d96ce
daudi44 <daudi@iesebre.com>
2022-04-21 16:09:21 +0200

    Fi video 138

--- app/Http/Controllers/SeriesImageManageController.php
+++ app/Http/Controllers/SeriesImageManageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Serie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Validation\Rule;
 use Tests\Feature\Series\SeriesImageManageControllerTest;
 
 class SeriesImageManageController extends Controller
@@ -14,6 +15,11 @@ class SeriesImageManageController extends Controller
     }
 
     public static function update(Request $request){
+
+        $request->validate([
+            'image'=>['image', 'dimensions:min_height=400,ratio=16/9,max_size:2000'],
+        ]);
+
         $serie = Serie::findOrFail($request->id);
 
         $serie->image = $request->file('image')->store('series','public');
--- tests/Feature/Series/SeriesImageManageControllerTest.php
+++ tests/Feature/Series/SeriesImageManageControllerTest.php
@@ -35,7 +35,7 @@ class SeriesImageManageControllerTest extends TestCase
 
         // URI ENDPOINT -> API -> FUNCTION
         $response = $this->put('/manage/series/' . $serie->id . '/image/',[
-            'image' => $file = UploadedFile::fake()->image('test.png'),
+            'image' => $file = UploadedFile::fake()->image('test.png', 960, 540),
         ]);
 
         $response->assertRedirect();
@@ -45,6 +45,120 @@ class SeriesImageManageControllerTest extends TestCase
         $response->assertSessionHas('status', __('Successfully updated'));
 
         $this->assertEquals($serie->refresh()->image,'series/'.$file->hashName());
-        $this->assertNotNull($serie->refresh()->image);
+        $this->assertNotNull($serie->image);
+
+        $this->assertFileEquals($file->getPathname(),Storage::disk('public')->path($serie->image));
     }
+
+
+    /**
+     * @test
+     */
+    public function series_image_have_to_be_an_image()
+    {
+        $this->loginAsSeriesManager();
+
+        $serie = Serie::create([
+            'title' => 'test',
+            'description' => 'test',
+            'image' => 'series/test.png',
+            'teacher_name' => 'Dani Audí'
+        ]);
+
+        Storage::fake('public');
+
+        $response = $this->put('/manage/series/' . $serie->id . '/image/',[
+            'image' => $file = UploadedFile::fake()->create('test.pdf',0,'application/pdf'),
+        ]);
+
+        $response->assertRedirect();
+
+        $response->assertSessionHasErrors('image');
+
+        $this->assertEquals('series/test.png',$serie->refresh()->image);
+    }
+
+
+    /**
+     * @test
+     */
+    public function series_image_must_be_at_least_400px_height()
+    {
+        $this->loginAsSeriesManager();
+
+        $serie = Serie::create([
+            'title' => 'test',
+            'description' => 'test',
+            'image' => 'series/test.png',
+            'teacher_name' => 'Dani Audí'
+        ]);
+
+        Storage::fake('public');
+
+        $response = $this->put('/manage/series/' . $serie->id . '/image/',[
+            'image' => $file = UploadedFile::fake()->image('serie.jpg', 200, 399),
+        ]);
+
+        $response->assertRedirect();
+
+        $response->assertSessionHasErrors('image');
+
+        $this->assertEquals('series/test.png',$serie->refresh()->image);
+    }
+
+    /**
+     * @test
+     */
+    public function series_image_must_be_aspect_ratio_16_9()
+    {
+        $this->loginAsSeriesManager();
+
+        $serie = Serie::create([
+            'title' => 'test',
+            'description' => 'test',
+            'image' => 'series/test.png',
+            'teacher_name' => 'Dani Audí'
+        ]);
+
+        Storage::fake('public');
+
+        $response = $this->put('/manage/series/' . $serie->id . '/image/',[
+            'image' => $file = UploadedFile::fake()->image('serie.jpg', 6000, 400),
+        ]);
+
+        $response->assertRedirect();
+
+        $response->assertSessionHasErrors('image');
+
+        $this->assertEquals('series/test.png',$serie->refresh()->image);
+    }
+
+
+    //TEST PER COMPROVAR EL TAMANY DELS ARXIUS, PERÒ FALLA, O BÈ EL MÈTODE size(), O LA VALIDACIÓ FETA AL CONTROLADOR
+//    /**
+//     * @test
+//     */
+//    public function series_image_size_must_be_under_2_mb()
+//    {
+//        $this->loginAsSeriesManager();
+//
+//        $serie = Serie::create([
+//            'title' => 'test',
+//            'description' => 'test',
+//            'image' => 'series/test.png',
+//            'teacher_name' => 'Dani Audí'
+//        ]);
+//
+//        Storage::fake('public');
+//
+//        $response = $this->put('/manage/series/' . $serie->id . '/image/',[
+//            'image' => $file = UploadedFile::fake()->image('serie.jpg', 960, 540)->size(3000),
+//        ]);
+//
+//        $response->assertRedirect();
+//
+//        $response->assertSessionHasErrors('image');
+//
+//        $this->assertEquals('series/test.png',$serie->refresh()->image);
+//    }
 }
